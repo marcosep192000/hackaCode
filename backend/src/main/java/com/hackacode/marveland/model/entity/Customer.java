3672eccbd50e141a5ca31fd696c84f41aec791ec
marcos <marcosep192000@hotmail.com>
2023-06-09 20:56:20 -0300

    :sparkles: feat: create Entity Customer

---
+++ backend/src/main/java/com/hackacode/marveland/model/entity/Customer.java
@@ -0,0 +1,38 @@
+package com.hackacode.marveland.model.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "customer")
+public class Customer {
+	@Id
+	@Column(name = "id", nullable = false)
+	private Long id;
+	@NotBlank(message = "this field can not be blank")
+	@Column(name = "FIRST_NAME")
+	private String firstName;
+
+	@NotBlank(message = "this field can not be blank")
+	@Column(name = "LAST_NAME")
+	private String lastName;
+
+	@NotBlank(message = "this field can not be blank")
+	@Column(name = "dni")
+	private  int dni;
+
+	@NotBlank(message = "this field can not be blank")
+	@Column(name = "EMAIL")
+	private String email;
+
+	@NotBlank(message = "this field can not be blank")
+	@Column(name = "BIRTH_DATE")
+	private String birthDate;
+}
