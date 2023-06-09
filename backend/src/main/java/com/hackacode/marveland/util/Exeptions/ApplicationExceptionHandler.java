3ad972bd095f6e7fddc3a55115b804b05bc5186f
marcos <marcosep192000@hotmail.com>
2023-06-09 20:54:37 -0300

    :sparkles: feat: create A

---
+++ backend/src/main/java/com/hackacode/marveland/util/Exeptions/ApplicationExceptionHandler.java
@@ -0,0 +1,32 @@
+package com.hackacode.marveland.util.Exeptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestControllerAdvice
+public class ApplicationExceptionHandler {
+	@ResponseStatus(HttpStatus.BAD_REQUEST)
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public Map<String, String> handleValidationExceptions(MethodArgumentNotValidException ex) {
+		Map<String, String> errors = new HashMap<>();
+		ex.getBindingResult().getAllErrors().forEach((error) -> {
+			String fieldName = ((FieldError) error).getField();
+			String errorMessage = error.getDefaultMessage();
+			errors.put(fieldName, errorMessage);
+		});
+		return errors;
+	}
+	@ExceptionHandler(RuntimeException.class)
+	public ResponseEntity<ErrorDto> runtimeException(RuntimeException exception) {
+		ErrorDto errorDto = ErrorDto.builder().message(exception.getMessage()).build();
+		return new ResponseEntity(errorDto, HttpStatus.BAD_REQUEST);
+	}
+}
