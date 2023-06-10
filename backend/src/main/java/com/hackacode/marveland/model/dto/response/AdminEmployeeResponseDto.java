f92fa562d24d15f6f2a1b8f2333eab9a9c8e9ff7
marcos <marcosep192000@hotmail.com>
2023-06-09 22:12:26 -0300

    :sparkles: create class AdminEmployeeResponseDto

---
+++ backend/src/main/java/com/hackacode/marveland/model/dto/response/AdminEmployeeResponseDto.java
@@ -0,0 +1,29 @@
+package com.hackacode.marveland.model.dto.response;
+
+import com.hackacode.marveland.model.entity.Customer;
+import jakarta.persistence.Column;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class AdminEmployeeResponseDto {
+
+	@Column(name = "EMPLOYEE_ID")
+	private Long id;
+
+	@Column(name = "FIRST_NAME")
+	private String firstName;
+
+	@Column(name = "LAST_NAME")
+	private String lastName;
+
+
+	List<Customer> listCustomer = new ArrayList<>();
+}
