8ae04a06b07f557191bcef8e01c55ad4558deb30
marcos <marcosep192000@hotmail.com>
2023-06-09 22:06:58 -0300

    :sparkles: create class CustomerRepository

---
+++ backend/src/main/java/com/hackacode/marveland/repository/CustomerRepository.java
@@ -0,0 +1,7 @@
+package com.hackacode.marveland.repository;
+
+import com.hackacode.marveland.model.entity.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CustomerRepository extends JpaRepository<Customer, Long> {
+}
