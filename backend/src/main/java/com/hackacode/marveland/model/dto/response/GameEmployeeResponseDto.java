a3e4e488e430bac391c17b909b985ade92308af1
marcos <marcosep192000@hotmail.com>
2023-06-09 22:10:20 -0300

    :sparkles: create Interface AdminEmployeeRepository

---
+++ backend/src/main/java/com/hackacode/marveland/model/dto/response/GameEmployeeResponseDto.java
@@ -0,0 +1,22 @@
+package com.hackacode.marveland.model.dto.response;
+
+import jakarta.persistence.Column;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class GameEmployeeResponseDto {
+	@Column(name = "EMPLOYEE_ID")
+	private Long id;
+
+	@Column(name = "FIRST_NAME")
+	private String firstName;
+
+	@Column(name = "LAST_NAME")
+	private String lastName;
+}
