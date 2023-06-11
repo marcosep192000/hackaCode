eed5a3d59e0cbc3ed9dc3d720e3c2af2648bc77d
marcos <marcosep192000@hotmail.com>
2023-06-11 04:40:25 -0300

    :sparkles: feat : add IAdminEmployeeService

--- backend/src/main/java/com/hackacode/marveland/service/IAdminEmployeeService.java
+++ backend/src/main/java/com/hackacode/marveland/service/IAdminEmployeeService.java
@@ -1,4 +1,20 @@
 package com.hackacode.marveland.service;
 
+import com.hackacode.marveland.model.dto.request.AdminEmployeeRequestDto;
+import com.hackacode.marveland.model.dto.response.AdminEmployeeResponseDto;
+import com.hackacode.marveland.model.dto.response.GameResponseDto;
+import com.hackacode.marveland.model.entity.AdminEmployee;
+
+import java.util.List;
+
 public interface IAdminEmployeeService {
+	void create(AdminEmployeeRequestDto requestDto);
+	void Update(AdminEmployeeRequestDto requestDto, AdminEmployee adminEmployee);
+
+	public List<AdminEmployeeResponseDto> getAll();
+
+	public AdminEmployeeResponseDto getById(Long id);
+
+	public void delete(Long id);
+
 }
