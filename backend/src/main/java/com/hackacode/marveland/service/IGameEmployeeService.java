86db5b18adcfe8bf8f58eab493d540af337a57cc
marcos <marcosep192000@hotmail.com>
2023-06-11 04:40:08 -0300

    :sparkles: feat : add IGameEmployeeService

--- backend/src/main/java/com/hackacode/marveland/service/IGameEmployeeService.java
+++ backend/src/main/java/com/hackacode/marveland/service/IGameEmployeeService.java
@@ -1,4 +1,21 @@
 package com.hackacode.marveland.service;
 
+import com.hackacode.marveland.model.dto.request.CustomerRequestDto;
+import com.hackacode.marveland.model.dto.request.GameEmployeeRequestDto;
+import com.hackacode.marveland.model.dto.response.CustomerResponseDto;
+import com.hackacode.marveland.model.dto.response.GameEmployeeResponseDto;
+import com.hackacode.marveland.model.entity.Customer;
+import com.hackacode.marveland.model.entity.GameEmployee;
+
+import java.util.List;
+
 public interface IGameEmployeeService {
+	void create(GameEmployeeRequestDto requestDto);
+	void Update(GameEmployeeRequestDto requestDto, GameEmployee request);
+
+	public List<GameEmployeeResponseDto> getAll();
+
+	public GameEmployeeResponseDto getById(Long id);
+
+	public void delete(Long id);
 }
