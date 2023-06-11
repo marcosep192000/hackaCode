33c9e26dc53826f33a74033845903bfe715827a3
marcos <marcosep192000@hotmail.com>
2023-06-11 04:40:55 -0300

    :sparkles: feat : add CustomerRepository

--- backend/src/main/java/com/hackacode/marveland/repository/CustomerRepository.java
+++ backend/src/main/java/com/hackacode/marveland/repository/CustomerRepository.java
@@ -4,4 +4,5 @@ import com.hackacode.marveland.model.entity.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
+	Customer findByDni(int dni);
 }
