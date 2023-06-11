afa9b2876167549090a884b1b84d321817037f88
marcos <marcosep192000@hotmail.com>
2023-06-11 04:39:45 -0300

    :sparkles: feat : add ICustomerService

--- backend/src/main/java/com/hackacode/marveland/service/ICustomerService.java
+++ backend/src/main/java/com/hackacode/marveland/service/ICustomerService.java
@@ -1,4 +1,19 @@
 package com.hackacode.marveland.service;
 
+import com.hackacode.marveland.model.dto.request.CustomerRequestDto;
+import com.hackacode.marveland.model.dto.response.CustomerResponseDto;
+import com.hackacode.marveland.model.entity.Customer;
+
+import java.util.List;
+
 public interface ICustomerService {
+	void create(CustomerRequestDto requestDto);
+	Customer Update(CustomerRequestDto requestDto, Long id );
+
+	public List<CustomerResponseDto> getAll();
+
+	public CustomerResponseDto getById(Long id);
+
+	public CustomerResponseDto getByDni(int dni);
+	public void delete(Long id);
 }
