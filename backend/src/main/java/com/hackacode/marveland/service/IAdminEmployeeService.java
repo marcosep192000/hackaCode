7518aad852cd7fea4ddd351826d30b71846e23c1
marcos <marcosep192000@hotmail.com>
2023-06-11 22:15:46 -0300

    :bug: fix: modify IAdminEmployeeService

--- backend/src/main/java/com/hackacode/marveland/service/IAdminEmployeeService.java
+++ backend/src/main/java/com/hackacode/marveland/service/IAdminEmployeeService.java
@@ -2,18 +2,18 @@ package com.hackacode.marveland.service;
 
 import com.hackacode.marveland.model.dto.request.AdminEmployeeRequestDto;
 import com.hackacode.marveland.model.dto.response.AdminEmployeeResponseDto;
-import com.hackacode.marveland.model.dto.response.GameResponseDto;
 import com.hackacode.marveland.model.entity.AdminEmployee;
 
 import java.util.List;
 
 public interface IAdminEmployeeService {
-	void create(AdminEmployeeRequestDto requestDto);
-	void Update(AdminEmployeeRequestDto requestDto, AdminEmployee adminEmployee);
 
-	public List<AdminEmployeeResponseDto> getAll();
-
+	void Update(AdminEmployeeRequestDto requestDto, Long id);
+	public List<AdminEmployee> getAllAdminCustomer();
+	public List<AdminEmployeeResponseDto> getAllAdmin();
 	public AdminEmployeeResponseDto getById(Long id);
+
+	public AdminEmployeeResponseDto getByDni(int dni);
 
 	public void delete(Long id);
 
