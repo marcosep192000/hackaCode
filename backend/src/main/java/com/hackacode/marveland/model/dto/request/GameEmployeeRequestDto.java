2fb32bc87be2219529679f51c314a4b8778e0636
Marcos Paz Goncheff <69874820+marcosep192000@users.noreply.github.com>
2023-06-15 12:23:33 -0300

    :lock: fix : add Exceptions

--- backend/src/main/java/com/hackacode/marveland/model/dto/request/GameEmployeeRequestDto.java
+++ backend/src/main/java/com/hackacode/marveland/model/dto/request/GameEmployeeRequestDto.java
@@ -1,5 +1,6 @@
 package com.hackacode.marveland.model.dto.request;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -10,16 +11,16 @@ import lombok.Setter;
 @AllArgsConstructor
 @NoArgsConstructor
 public class GameEmployeeRequestDto {
-
+    @NotBlank(message = "this field can not be blank")
     private String firstName;
-
+    @NotBlank(message = "this field can not be blank")
     private String lastName;
 
     private Integer dni;
-
+    @NotBlank(message = "this field can not be blank")
     private String email;
-
+    @NotBlank(message = "this field can not be blank")
     private String password;
-
+    @NotBlank(message = "this field can not be blank")
     private String workingHours;
 }
