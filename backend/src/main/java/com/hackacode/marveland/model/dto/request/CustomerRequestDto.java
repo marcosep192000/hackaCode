19607c5417373adf9acae9ec25fb9de609ac95f1
Marcos Paz Goncheff <69874820+marcosep192000@users.noreply.github.com>
2023-06-15 12:00:06 -0300

    :bug: fix : add Exception of CustomerRequestDto

--- backend/src/main/java/com/hackacode/marveland/model/dto/request/CustomerRequestDto.java
+++ backend/src/main/java/com/hackacode/marveland/model/dto/request/CustomerRequestDto.java
@@ -2,6 +2,9 @@ package com.hackacode.marveland.model.dto.request;
 
 import java.io.Serializable;
 
+import jakarta.persistence.Column;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -15,15 +18,15 @@ import lombok.Setter;
 @NoArgsConstructor
 public class CustomerRequestDto implements Serializable {
 
+	@NotNull(message = "this field can not be blank")
 	private String firstName;
-
+	@NotBlank(message = "this field can not be blank")
 	private String lastName;
-
 	private Integer dni;
-
+	@NotBlank(message = "this field can not be blank")
 	private String email;
-
+	@NotBlank(message = "this field can not be blank")
 	private String phone;
-
+	@NotBlank(message = "this field can not be blank")
 	private String birthdate;
 }
