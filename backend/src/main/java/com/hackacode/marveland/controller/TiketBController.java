1940790719af757108613c01c9cd39023167f377
Marcos Paz Goncheff <69874820+marcosep192000@users.noreply.github.com>
2023-07-06 14:48:50 -0300

    :sparkles: fix: errors

--- backend/src/main/java/com/hackacode/marveland/controller/TiketBController.java
+++ backend/src/main/java/com/hackacode/marveland/controller/TiketBController.java
@@ -8,9 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
 import java.util.List;
-
 @RequestMapping("/api/v1/tiket-b")
 @CrossOrigin(origins = "*")
 @RestController
@@ -28,7 +26,4 @@ public class TiketBController {
 		List<TiketB> tiket = tiketBService.getAll();
 		return ResponseEntity.ok(tiket);
 	}
-
-
-
 }
